ca77ab2b02046cd47d5824f07e99bd597176a283
Marcus Little <mcolelittle@gmail.com>
2018-12-28 12:04:03 -0700

    assignment-express-intro updated

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -16,7 +16,7 @@ describe("routes : static", () => {
 
     describe("GET /marco", () => {
         it("should return a status code 200", (done) => {
-            request.get(base, (err, res, body) => {
+            request.get(base + "marco", (err, res, body) => {
                 expect(res.statusCode).toBe(200);
                 expect(body).toBe("polo");
                 done();
