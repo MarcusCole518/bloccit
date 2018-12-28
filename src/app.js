1379e0b8117b692f8f4f58fc14538d989ce8c5ab
Marcus Little <mcolelittle@gmail.com>
2018-12-28 10:00:48 -0700

    WIP deploy to production

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
+const appConfig = require("./config/main-config.js");
 
-
+appConfig.init();
 const routeConfig = require("./config/route-config.js");
 routeConfig.init(app);
 
