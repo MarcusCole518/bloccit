0b21021a585f68c32b4a2ee13f9f101871666e7d
Marcus Little <mcolelittle@gmail.com>
2019-01-06 17:30:52 -0700

    complete assignment-topic-resource-2

--- spec/integration/ad_spec.js
+++ spec/integration/ad_spec.js
@@ -36,4 +36,106 @@ describe("routes : advertisements", () => {
             });
         });
     });
+
+    describe("GET /advertisements/new", () => {
+        it("should render a new advertisement form", (done) => {
+            request.get(`${base}new`, (err, res, body) => {
+                expect(err).toBeNull();
+                expect(body).toContain("New Advertisement");
+                done();
+            });
+        });
+    });
+
+    describe("POST /advertisements/create", () => {
+        const options = {
+            url: `${base}create`,
+            form: {
+                title: "Pizza",
+                description: "What's your favorite flavor of pizza?"
+            }
+        };
+
+        it("should create a new advertisement and redirect", (done) => {
+            request.post(options, (err, res, body) => {
+                Advertisement.findOne({where: {title: "Pizza"}})
+                .then((advertisement) => {
+                    expect(res.statusCode).toBe(303);
+                    expect(advertisement.title).toBe("Pizza");
+                    expect(advertisement.description).toBe("What's your favorite flavor of pizza?");
+                    done();
+                })
+                .catch((err) => {
+                    console.log(err);
+                    done();
+                });
+            });
+        });
+    });
+
+    describe("GET /advertisements/:id", () => {
+        it("should render a view with the selected advertisement", (done) => {
+            request.get(`${base}${this.advertisement.id}`, (err, res, body) => {
+                expect(err).toBeNull();
+                expect(body).toContain("Verb");
+                done();
+            });
+        });
+    });
+
+    describe("POST /advertisements/:id/destroy", () => {
+        it("should delete the advertisement with the associated ID", (done) => {
+
+            Advertisement.all()
+            .then((advertisements) => {
+
+                const adCountBeforeDelete = advertisements.length;
+                expect(adCountBeforeDelete).toBe(1);
+
+                request.post(`${base}${this.advertisement.id}/destroy`, (err, res, body) => {
+                    Advertisement.all()
+                    .then((advertisements) => {
+                        expect(err).toBeNull();
+                        expect(advertisements.length).toBe(adCountBeforeDelete - 1);
+                        done();
+                    })
+                });
+            });
+        });
+    });
+
+    describe("GET /advertisements/:id/edit", () => {
+        it("should render a view with an edit advertisement form", (done) => {
+            request.get(`${base}${this.advertisement.id}/edit`, (err, res, body) => {
+                expect(err).toBeNull();
+                expect(body).toContain("Edit Advertisement");
+                expect(body).toContain("Verb");
+                done();
+            });
+        });
+    });
+
+    describe("POST /advertisements/:id/update", () => {
+        it("should update the advertisement with the given values", (done) => {
+            const options = {
+                url: `${base}${this.advertisement.id}/update`,
+                form: {
+                    title: "Noun",
+                    description: "Say what you mean"
+                }
+            };
+
+            request.post(options, (err, res, body) => {
+                expect(err).toBeNull();
+
+                Advertisement.findOne({
+                    where: {id: this.advertisement.id}
+                })
+                .then((advertisement) => {
+                    expect(advertisement.title).toBe("Noun");
+                    done();
+                });
+            });
+        });
+    });
 });
--- src/controllers/adController.js
+++ src/controllers/adController.js
@@ -9,5 +9,64 @@ module.exports = {
                 res.render("advertisements/index", {advertisements});
             }
         })
+    },
+
+    new(req, res, next){
+        res.render("advertisements/new");
+    },
+
+    create(req, res, next){
+        let newAdvertisement = {
+            title: req.body.title,
+            description: req.body.description
+        };
+        adQueries.addAdvertisement(newAdvertisement, (err, advertisement) => {
+            if(err){
+                res.redirect(500, "/advertisements/new");
+            } else {
+                res.redirect(303, `/advertisements/${advertisement.id}`);
+            }
+        });
+    },
+
+    show(req, res, next){
+        adQueries.getAdvertisement(req.params.id, (err, advertisement) => {
+            if(err || advertisement == null){
+                res.redirect(404, "/");
+            } else {
+                res.render("advertisements/show", {advertisement});
+            }
+        })
+    },
+
+    destroy(req, res, next){
+        adQueries.deleteAdvertisement(req.params.id, (err, advertisement) => {
+            if(err){
+                res.redirect(500, `/advertisements/${advertisement.id}`)
+            } else {
+                res.redirect(303, "/advertisements")
+            }
+        })
+    },
+
+    edit(req, res, next){
+        adQueries.getAdvertisement(req.params.id, (err, advertisement) => {
+            if(err || advertisement == null){
+                res.redirect(404, "/");
+            } else {
+                res.render("advertisements/edit", {advertisement});
+            }
+        });
+    },
+
+    update(req, res, next){
+        adQueries.updateAdvertisement(req.params.id, req.body, (err, advertisement) => {
+
+            if(err || advertisement == null){
+                res.redirect(404, `/advertisements/${req.params.id}/edit`);
+            } else {
+                res.redirect(`/advertisements/${advertisement.id}`);
+            }
+        });
     }
 }
