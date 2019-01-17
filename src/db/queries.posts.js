04c5e6e2254a7e5dc0149971f2177f995e1ca27d
Marcus Little <mcolelittle@gmail.com>
2019-01-16 23:01:54 -0700

    Revert "complete assignment-authorization"
    This reverts commit adae44d0d29c2b3b07e4223428b7a99b931513d3.

--- src/db/queries.posts.js
+++ src/db/queries.posts.js
@@ -1,6 +1,5 @@
 const Post = require("./models").Post;
 const Topic = require("./models").Topic;
-const Authorizer = require("../policies/post");
 
 module.exports = {
 
@@ -24,50 +23,33 @@ module.exports = {
         })
     },
 
-    deletePost(req, callback){
-        return Post.findById(req.params.id)
-        .then((post) => {
-
-            const authorized = new Authorizer(req.user, post).destroy();
-
-            if(authorized){
-                post.destroy()
-                .then((res) => {
-                    callback(null, post);
-                });
-            } else {
-                req.flash("notice", "You are not authorized to do that.")
-                callback(401);
-            }
+    deletePost(id, callback){
+        return Post.destroy({
+            where: {id}
+        })
+        .then((deleteRecordsCount) => {
+            callback(null, deleteRecordsCount);
         })
         .catch((err) => {
             callback(err);
-        });
+        })
     },
 
-    updatePost(req, updatedPost, callback){
-        return Post.findById(req.params.id)
+    updatePost(id, updatedPost, callback){
+        return Post.findById(id)
         .then((post) => {
             if(!post){
                 return callback("Post not found");
             }
-
-            const authorized = new Authorizer(req.user, post).update();
-
-            if(authorized) {
-                post.update(updatedPost, {
-                    fields: Object.keys(updatedPost)
-                })
-                .then(() =>{
-                    callback(null, post);
-                })
-                .catch((err) => {
-                    callback(err);
-                });
-            } else {
-                req.flash("notice", "You are not authorized to do that.");
-                callback("Forbidden");
-            }
+            post.update(updatedPost, {
+                fields: Object.keys(updatedPost)
+            })
+            .then(() =>{
+                callback(null, post);
+            })
+            .catch((err) => {
+                callback(err);
+            });
         });
     }
 }
