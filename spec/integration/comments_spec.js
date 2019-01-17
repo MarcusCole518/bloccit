9788ac997ee39753abb7c076c400bcce3e60776e
Marcus Little <mcolelittle@gmail.com>
2019-01-17 13:14:31 -0700

    complete assignment-comments

--- spec/integration/comments_spec.js
+++ spec/integration/comments_spec.js
@@ -183,6 +183,77 @@ describe("routes : comments", () => {
                         });
                 })
             });
+
+            it("should not delete a comment with the another user's ID", (done) => {
+
+                request.get({
+                    url: "http://localhost:3000/auth/fake",
+                    form: {
+                        role: "member",
+                        userId: 3
+                    }
+                }, (err, res, body) => {
+                    done();
+                })
+
+                Comment.all()
+                .then((comments) => {
+                    const commentCountBeforeDelete = comments.length;
+
+                    expect(commentCountBeforeDelete).toBe(1);
+
+                    request.post(
+                        `${base}${this.topic.id}/posts/${this.post.id}/comments/${this.comment.id}/destroy`,
+                        (err, res, body) => {
+                            Comment.all()
+                            .then((comments) => {
+                                expect(err).toBeNull();
+                                expect(comments.length).toBe(commentCountBeforeDelete);
+                                done();
+                        })
+                    });
+                })
+            });
         });
-    });;
+    });
+
+    //admin actions
+
+    describe("signed in admin performing CRUD actions on Comment", () => {
+
+        beforeEach((done) => {
+            request.get({
+                url: "http://localhost:3000/auth/fake",
+                form: {
+                    role: "admin",
+                    userId: 4
+                }
+            }, (err, res, body) => {
+                done();
+            })
+        });
+
+        describe("POST /topics/:topicId/posts/:postId/comments/:id/destroy", () => {
+            it("should delete the comment with the associated ID", (done) => {
+                Comment.all()
+                .then((comments) => {
+                    const commentCountBeforeDelete = comments.length;
+
+                    expect(commentCountBeforeDelete).toBe(1);
+
+                    request.post(
+                        `${base}${this.topic.id}/posts/${this.post.id}/comments/${this.comment.id}/destroy`,
+                        (err, res, body) => {
+                            expect(res.statusCode).toBe(302);
+                            Comment.all()
+                            .then((comments) => {
+                                expect(err).toBeNull();
+                                expect(comments.length).toBe(commentCountBeforeDelete - 1);
+                                done();
+                            })
+                        });
+                })
+            });
+        });
+    })
 });
