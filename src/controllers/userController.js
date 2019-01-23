0ee5f95e09b074847a39c42da87bd4f70a364804
Marcus Little <mcolelittle@gmail.com>
2019-01-23 11:38:01 -0700

    complete checkpoint-public-profiles

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,7 +2,7 @@ const userQueries = require("../db/queries.users.js");
 const passport = require("passport");
 
 module.exports = {
-    
+
     signUp(req, res, next){
         res.render("users/sign_up");
     },
@@ -51,5 +51,18 @@ module.exports = {
         req.logout();
         req.flash("notice", "You've successfully logged out!");
         res.redirect("/");
+    },
+
+    show(req, res, next){
+        userQueries.getUser(req.params.id, (err, result) => {
+
+            if(err || result.user === undefined){
+                req.flash("notice", "No user found with that ID.");
+                res.redirect("/");
+            } else {
+
+                res.render("users/show", {...result});
+            }
+        });
     }
 }
