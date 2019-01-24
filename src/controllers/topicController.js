e9aa8322f98522771e9020ead49bed38e64fafaf
Marcus Little <mcolelittle@gmail.com>
2019-01-24 12:05:44 -0700

    fixed issue where created method wasn't redirecting

--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -38,7 +38,7 @@ module.exports = {
                 if(err) {
                     res.redirect(500, "topics/new");
                 } else {
-                    res.redirect(303, `topics/${topic.id}`);
+                    res.redirect(303, `${topic.id}`);
                 }
             });
         } else {
